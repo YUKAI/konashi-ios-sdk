4985773595b2e7a440cf53627bd6905487036297
Monakaz <contact@ux-xu.com>
2013-02-16 01:36:22 +0900

    Added GettingStarted code.

---
+++ samples/GettingStarted/GettingStarted/ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  GettingStarted
+//
+//  Copyright (c) 2013 Yukai Engineering. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Konashi.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    [Konashi initialize];
+    
+    [Konashi addObserver:self selector:@selector(ready) name:KONASHI_EVENT_READY];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)find:(id)sender {
+    [Konashi find];
+}
+
+- (void)ready
+{
+    [Konashi pinMode:LED2 mode:OUTPUT];
+    [Konashi digitalWrite:LED2 value:HIGH];
+}
+
+@end
