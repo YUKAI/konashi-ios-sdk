fc22b8d6748cee804848215fc1368a3e25bd7f89
Akira Matsuda <akira.m.itachi@gmail.com>
2014-09-02 16:24:07 +0900

    Separating UI code from logic code.

---
+++ Konashi/Konashi+UI.h
@@ -0,0 +1,16 @@
+//
+//  Konashi+UI.h
+//  GettingStarted
+//
+//  Created by Akira Matsuda on 9/2/14.
+//  Copyright (c) 2014 Yukai Engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Konashi.h"
+
+@interface Konashi (UI) <UIActionSheetDelegate>
+
+- (void)showModulePicker;
+
+@end
