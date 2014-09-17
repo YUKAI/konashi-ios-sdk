cf93950694aea18353701269e72d2208575e764a
Akira Matsuda <akira.m.itachi@gmail.com>
2014-09-17 11:12:25 +0900

    fix conflict

---
+++ Konashi/KonashiTests/KonashiTests.m
@@ -0,0 +1,34 @@
+//
+//  KonashiTests.m
+//  KonashiTests
+//
+//  Created by Akira Matsuda on 9/17/14.
+//  Copyright (c) 2014 Akira Matsuda. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KonashiTests : XCTestCase
+
+@end
+
+@implementation KonashiTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
