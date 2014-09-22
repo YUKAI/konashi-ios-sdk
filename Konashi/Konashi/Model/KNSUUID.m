4e553c6b02780af19e3f0ec85550e4b6d725396a
Akira Matsuda <akira.m.itachi@gmail.com>
2014-09-22 17:47:29 +0900

    add new method

--- Konashi/Konashi/Model/KNSUUID.m
+++ Konashi/Konashi/Model/KNSUUID.m
@@ -7,3 +7,28 @@
 //
 
 #import "KNSUUID.h"
+
+KNSUUID uuidWithUInt16(UInt16 uuid16)
+{
+	KNSUUID uuid = {uuid16};
+	return uuid;
+}
+
+KNSUUID uuidWithCharacter(char *uuid128)
+{
+	KNSUUID uuid = {0, *uuid128};
+	return uuid;
+}
+
+CBUUID *convetToCBUUID(KNSUUID uuid)
+{
+	NSString *uuidString = nil;
+	if (uuid.uuid16) {
+		uuidString = [NSString stringWithFormat:@"%d", uuid.uuid16];
+	}
+	else if (uuid.uuid128) {
+		uuidString = [NSString stringWithUTF8String:uuid.uuid128];
+	}
+	
+	return [CBUUID UUIDWithString:uuidString];
+}
