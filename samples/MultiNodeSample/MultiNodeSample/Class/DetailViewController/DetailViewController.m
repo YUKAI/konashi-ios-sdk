d276afba74a1527f513081d0440975904d6f632e
Akira Matsuda <akira.m.itachi@gmail.com>
2014-12-05 02:28:07 +0900

    remove category

---
+++ samples/MultiNodeSample/MultiNodeSample/Class/DetailViewController/DetailViewController.m
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.m
+//  MultiNodeSample
+//
+//  Created by Akira Matsuda on 12/5/14.
+//  Copyright (c) 2014 Akira Matsuda. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@implementation DetailViewController
+
+@end
