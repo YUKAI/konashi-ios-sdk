ae2c890f262ff6942cc60329081fd787890816b5
Takashi Sagisaka <sagisaka@ux-xu.com>
2015-08-04 23:58:08 +0900

    added GettingStarted xcodeproj

---
+++ samples/GettingStarted/GettingStarted/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GettingStarted
+//
+//  Created by sagiii on 2015/08/04.
+//  Copyright (c) 2015年 sagiii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ samples/GettingStarted/GettingStarted/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  GettingStarted
+//
+//  Created by sagiii on 2015/08/04.
+//  Copyright (c) 2015年 sagiii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)find:(id)sender;   // add this line
+
+@end
+
